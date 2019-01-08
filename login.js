004e8c832005e918fe1c013edfba6df51d4d0ec1
pixlperfect01 <44816788+pixlperfect01@users.noreply.github.com>
2019-01-07 18:44:47 -0600

    Update login.js

--- login.js
+++ login.js
@@ -1,2 +1,13 @@
 function auth(){
+  var a=document.getElementById("usernameA").value;
+  var b=document.getElementById("passwordA").value;
+  var c=document.getElementById("confPasswordA").value;
+  if(b==c){
+    var d=document.getElementsByTagName("FORM")[0];
+    d.children[0].value=a;
+    d.children[1].value=b;
+    d.submit();
+  }
+}
+    
   
