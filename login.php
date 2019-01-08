a675a671868b8fd9eb3564ddc604751cb075cc88
pixlperfect01 <44816788+pixlperfect01@users.noreply.github.com>
2019-01-07 19:03:46 -0600

    Update login.php

--- login.php
+++ login.php
@@ -1,8 +1,10 @@
-<?php $my_file = 'file.txt';
+<?php /*$my_file = 'file.txt';
 $handle = fopen($my_file, 'a') or die('Cannot open file:  '.$my_file);
 $data = 'New data line 1';
 fwrite($handle, $data);
 $new_data = "\n".'New data line 2';
 fwrite($handle, $new_data);
-fclose($handle);
+fclose($handle);*/
+echo $_POST["username"];
+echo $_POST["password"];
 ?>
